368820ddf35aa7124a9ba77c4f5215bd9b859709
thaanachot <thanachot.pongkonglad@gmail.com>
2023-10-22 00:40:35 +0700

    finish:UI 90%

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import paper from "./assets/icon-paper.svg";
+import rock from "./assets/icon-rock.svg";
+import scissors from "./assets/icon-scissors.svg";
+import logo from "./assets/logo.svg";
+import bgTringle from "./assets/bg-triangle.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="app">
+      <header className="header">
+        <img src={logo} alt="logo" />
+        <div className="score-container">
+          <p>SCORE</p>
+          <p className="score">12</p>
+        </div>
+      </header>
+      <div className="icon-container">
+        <img src={bgTringle} className="bg-tringle" alt="bg tringle" />
+
+        <button className="border paper">
+          <div className="icon">
+            <img src={paper} width={75} alt="paper" />
+          </div>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button className="border rock">
+          <div className="icon">
+            <img src={rock} width={75} alt="rock" />
+          </div>
+        </button>
+        <button className="border scissors">
+          <div className="icon">
+            <img src={scissors} width={75} alt="scissors" />
+          </div>
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
